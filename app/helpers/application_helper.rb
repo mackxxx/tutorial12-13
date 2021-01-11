10103b2ee36f7db2dfd0fede76a79c624495da43
mackxxx <42348939+mackxxx@users.noreply.github.com>
2021-01-12 01:00:55 +0900

    helper修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   # ページごとの完全なタイトルを返します。
-  def full_title(page_title = '')
+  def full_title(yield_title)
     base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
+    if yield_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      yield_title + " | " + base_title
     end
   end
 end
